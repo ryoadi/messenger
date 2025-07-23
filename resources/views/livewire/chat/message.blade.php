edb25cce540eb3a0e9986c52cf7b9b01df32cf22
han <han@voxteneo.com>
2025-07-24 06:21:19 +0700

    adapt message card
    Signed-off-by: han <han@voxteneo.com>

--- resources/views/livewire/chat/message.blade.php
+++ resources/views/livewire/chat/message.blade.php
@@ -7,7 +7,7 @@ new class extends Component {
     //
 }; ?>
 
-<div class="px-4 py-2 max-w-3/4 rounded-md {{ $own ? 'self-end dark:bg-zinc-600' : 'self-start dark:bg-zinc-700'}}">
-    <flux:heading>username</flux:heading>
+<div class="lg:max-w-3/4 px-2 py-2 rounded-md {{ $own ? 'self-end dark:bg-zinc-600' : 'self-start dark:bg-zinc-700'}}">
     <flux:text>the messages</flux:text>
+    <flux:text size="sm" variant="subtle" class="text-end"><time>10:00</time></flux:text>
 </div>
--- resources/views/pages/chat/[id].blade.php
+++ resources/views/pages/chat/[id].blade.php
@@ -9,8 +9,11 @@ new class extends Component {
 <x-layouts.app :title="__('Chat')">
     @volt
         <main class="flex flex-col h-full">
-            <div class="pb-2 grow flex flex-col-reverse gap-2">
+            <div class="pb-2 grow flex flex-col-reverse gap-3">
+                <!-- own -->
                 <livewire:chat.message :own="true" />
+
+                <!-- theirs -->
                 <livewire:chat.message />
             </div>
 
