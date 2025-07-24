430ac3a2bd92ba0626e7ba011ed89539c8121bbf
han <han@voxteneo.com>
2025-07-24 13:22:35 +0700

    add chat header
    Signed-off-by: han <han@voxteneo.com>

--- resources/views/pages/chat/[id].blade.php
+++ resources/views/pages/chat/[id].blade.php
@@ -8,8 +8,13 @@ new class extends Component {
 
 <x-layouts.app :title="__('Chat')">
     @volt
-        <main class="relative flex flex-col h-full">
-            <div class="pb-2 grow flex flex-col-reverse gap-3 overflow-y-auto">
+        <div class="relative flex flex-col h-full">
+            <header class="flex gap-2 items-center sticky lg:top-0 pt-6 pb-3 -mt-6 lg:-mt-8 z-10 dark:bg-zinc-800">
+                <flux:avatar circle name="username" />
+                <flux:heading size="xl"><flux:link href="#" variant="ghost">username</flux:link></flux:heading>
+            </header>
+
+            <main class="pb-2 grow flex flex-col-reverse gap-3 overflow-y-auto">
                 <!-- own -->
                 <livewire:chat.message :own="true" />
 
@@ -21,11 +26,11 @@ new class extends Component {
                 <flux:separator variant="subtle" text="{{ __('Start conversation') }}" />
 
                 <flux:button variant="subtle" size="sm" class="w-full">{{ __('Load more') }}</flux:button>
-            </div>
+            </main>
 
-            <div class="sticky bottom-0 pb-6 pt-2 -mb-6 lg:-mb-8 dark:bg-zinc-800">
+            <footer class="sticky bottom-0 pb-6 pt-2 -mb-6 lg:-mb-8 dark:bg-zinc-800">
                 <flux:input placeholder="{{ __('Say something...') }}" />
-            </div>
-        </main>
+            </footer>
+        </div>
     @endvolt
 </x-layouts.app>
