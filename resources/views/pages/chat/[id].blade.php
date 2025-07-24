757aa604093030dae0bb25998c28e202a7a85ea0
han <han@voxteneo.com>
2025-07-24 10:11:00 +0700

    scrollable content
    Signed-off-by: han <han@voxteneo.com>

--- resources/views/pages/chat/[id].blade.php
+++ resources/views/pages/chat/[id].blade.php
@@ -8,20 +8,24 @@ new class extends Component {
 
 <x-layouts.app :title="__('Chat')">
     @volt
-        <main class="flex flex-col h-full">
-            <div class="pb-2 grow flex flex-col-reverse gap-3">
+        <main class="relative flex flex-col h-full">
+            <div class="pb-2 grow flex flex-col-reverse gap-3 overflow-y-auto">
                 <!-- own -->
                 <livewire:chat.message :own="true" />
 
                 <!-- theirs -->
-                <livewire:chat.message />
+                 @for ($index = 0; $index < 20; $index++)
+                    <livewire:chat.message />
+                 @endfor
 
                 <flux:separator variant="subtle" text="{{ __('Start conversation') }}" />
 
                 <flux:button variant="subtle" size="sm" class="w-full">{{ __('Load more') }}</flux:button>
             </div>
 
-            <flux:input placeholder="{{ __('Say something...') }}" />
+            <div class="sticky bottom-0 pb-6 pt-2 -mb-6 lg:-mb-8 dark:bg-zinc-800">
+                <flux:input placeholder="{{ __('Say something...') }}" />
+            </div>
         </main>
     @endvolt
 </x-layouts.app>
