17a56f77b7fb5d9a5198228cebc078e15fcf711e
han <han@voxteneo.com>
2025-07-24 12:52:23 +0700

    message card, menu dropdown
    Signed-off-by: han <han@voxteneo.com>

--- resources/views/livewire/chat/message.blade.php
+++ resources/views/livewire/chat/message.blade.php
@@ -7,7 +7,24 @@ new class extends Component {
     //
 }; ?>
 
-<div class="lg:max-w-3/4 px-2 py-2 rounded-md {{ $own ? 'self-end dark:bg-zinc-600' : 'self-start dark:bg-zinc-700'}}">
-    <flux:text>the messages</flux:text>
-    <flux:text size="sm" variant="subtle" class="text-end"><time>10:00</time></flux:text>
+<div class="flex gap-2 [&_[data-open]]:block hover:[&_[data-flux-dropdown]]:block {{ $own ? 'flex-row-reverse' : '' }}">
+    <div class="px-2 py-2 rounded-md {{ $own ? 'dark:bg-zinc-600' : 'dark:bg-zinc-700'}}">
+        <flux:text>the messages</flux:text>
+        <flux:text size="sm" variant="subtle" class="text-end"><time>10:00</time></flux:text>
+    </div>
+
+    <flux:dropdown class="hidden">
+        <flux:button size="xs" variant="ghost" icon="ellipsis-vertical" />
+
+        <flux:menu>
+            @if ($own)
+                <flux:menu.item icon="pencil">{{ __('Edit') }}</flux:menu.item>
+                <flux:menu.item variant="danger" icon="trash">{{ __('Delete') }}</flux:menu.item>
+            @else
+                <flux:menu.item icon="arrow-uturn-left">{{ __('Reply') }}</flux:menu.item>
+                <flux:menu.item icon="arrow-uturn-right">{{ __('Forward') }}</flux:menu.item>
+                <flux:menu.item icon="face-smile">{{ __('React') }}</flux:menu.item>
+            @endif
+        </flux:menu>
+    </flux:dropdown>
 </div>
