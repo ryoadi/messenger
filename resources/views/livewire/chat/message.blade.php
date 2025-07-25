4e183a63a1f2c4e695d32e80fa05ce0691fb24d9
han <han@voxteneo.com>
2025-07-25 09:42:53 +0700

    delete message modal
    Signed-off-by: han <han@voxteneo.com>

--- resources/views/livewire/chat/message.blade.php
+++ resources/views/livewire/chat/message.blade.php
@@ -19,7 +19,9 @@ new class extends Component {
         <flux:menu>
             @if ($own)
                 <flux:menu.item icon="pencil">{{ __('Edit') }}</flux:menu.item>
-                <flux:menu.item variant="danger" icon="trash">{{ __('Delete') }}</flux:menu.item>
+                <flux:modal.trigger name="delete-message">
+                    <flux:menu.item variant="danger" icon="trash">{{ __('Delete') }}</flux:menu.item>
+                </flux:modal.trigger>
             @else
                 <flux:menu.item icon="arrow-uturn-left">{{ __('Reply') }}</flux:menu.item>
                 <flux:menu.item icon="arrow-uturn-right">{{ __('Forward') }}</flux:menu.item>
@@ -27,4 +29,18 @@ new class extends Component {
             @endif
         </flux:menu>
     </flux:dropdown>
+
+    @if ($own)
+        <flux:modal name="delete-message">
+            <flux:heading class="mb-2">{{ __('Delete message') }}</flux:heading>
+            <flux:text class="mb-4">{{ __('Are you sure you want to delete this message?') }}</flux:text>
+            <div class="flex gap-2 justify-end">
+                <flux:modal.close>
+                    <flux:button>{{ __('Cancel') }}</flux:button>
+                </flux:modal.close>
+                
+                <flux:button variant="danger">{{ __('Delete') }}</flux:button>
+            </div>
+        </flux:modal>
+    @endif
 </div>
