3fde87d40d437b75d893d97191c90485951a42ca
han <han@voxteneo.com>
2025-07-26 16:03:37 +0700

    add profile modal
    Signed-off-by: han <han@voxteneo.com>

--- resources/views/pages/chat/[id].blade.php
+++ resources/views/pages/chat/[id].blade.php
@@ -10,8 +10,19 @@ new class extends Component {
     @volt
         <div class="relative flex flex-col h-full">
             <header class="flex gap-2 items-center sticky top-0 pb-3 -mt-20 pt-15 lg:pt-6 lg:-mt-8 z-10 dark:bg-zinc-800">
-                <flux:avatar circle name="username" />
-                <flux:heading size="xl"><flux:link href="#" variant="ghost">username</flux:link></flux:heading>
+                <flux:modal.trigger name="profile-info">
+                    <flux:avatar circle name="username" />
+                    <flux:heading size="xl"><flux:link variant="ghost">username</flux:link></flux:heading>
+                </flux:modal.trigger>
+
+                <flux:modal name="profile-info" variant="flyout" position="right">
+                    <div class="flex flex-col gap-4 items-center">
+                        <flux:avatar circle name="username" size="xl" />
+                        <flux:heading size="xl">username</flux:heading>
+                        <flux:text>Introduction text</flux:text>
+                        <flux:text>Last seen: 10 minutes ago</flux:text>
+                    </div>
+                </flux:modal>
             </header>
 
             <main class="pb-2 grow flex flex-col-reverse gap-3 overflow-y-auto">
