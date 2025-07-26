e525d406f550e983705fec7868920cf009fbdcc1
han <han@voxteneo.com>
2025-07-26 13:14:40 +0700

    adapt room list scroller
    Signed-off-by: han <han@voxteneo.com>

--- resources/views/livewire/chat/list.blade.php
+++ resources/views/livewire/chat/list.blade.php
@@ -6,17 +6,19 @@ new class extends Component {
     //
 }; ?>
 
-<div class="relative flex flex-col h-full overflow-y-auto pr-2">
-    <div class="sticky top-0 py-1 dark:bg-zinc-900 z-10 space-y-2 pl-1">
+<div class="flex flex-col gap-2 h-full overflow-y-auto pr-2">
+    <div class="py-1 dark:bg-zinc-900 z-10 space-y-2 pl-1">
         <flux:button icon="plus" size="sm" class="w-full">{{ __('New') }}</flux:button>
         <flux:input inset size="sm" type="search" placeholder="{{ __('Search') }}" />
     </div>
 
-    @for ($index = 0; $index < 10; $index++)
+    <div class="overflow-y-auto">
+        @for ($index = 0; $index < 10; $index++)
         <a href="/chat/1" wire:navigate>
             <flux:profile :name="'username'" :chevron="false" class="w-full" />
         </a>
-    @endfor
+        @endfor
 
-    <flux:button variant="subtle" size="sm" class="w-full mt-2">{{ __('Load more') }}</flux:button>
+        <flux:button variant="subtle" size="sm" class="w-full mt-2">{{ __('Load more') }}</flux:button>
+    </div>
 </div>
