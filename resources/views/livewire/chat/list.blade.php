250b7660699d69be40a45ad2cd150919e9a58542
han <han@voxteneo.com>
2025-07-27 02:47:25 +0700

    update create modal width
    Signed-off-by: han <han@voxteneo.com>

--- resources/views/livewire/chat/list.blade.php
+++ resources/views/livewire/chat/list.blade.php
@@ -19,29 +19,32 @@ new class extends Component {
         </div>
     </div>
 
-    <flux:modal name="room-create" variant="flyout" position="left" class="[&[data-open]]:flex flex-col gap-2 max-w-1/4">
-        <flux:heading size="lg">{{ __('Create a new room') }}</flux:heading>
-        <div class="space-y-2">
-            <flux:button type="submit" variant="primary" size="sm" class="w-full">{{ __('Create') }}</flux:button>
-            <flux:input size="sm" placeholder="{{ __('Room name') }}" />
+    <flux:modal name="room-create" variant="flyout" position="left" class="h-dvh max-w-full md:max-w-1/4">
+        <div class="flex flex-col h-full gap-4">
+            <flux:heading size="lg">{{ __('Create a new room') }}</flux:heading>
 
-            <div class="flex gap-2 flex-wrap">
-                <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
-                <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
-                <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
-                <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
-                <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
+            <div class="space-y-2">
+                <flux:button type="submit" variant="primary" size="sm" class="w-full">{{ __('Create') }}</flux:button>
+                <flux:input size="sm" placeholder="{{ __('Room name') }}" />
+
+                <div class="flex gap-2 flex-wrap">
+                    <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
+                    <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
+                    <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
+                    <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
+                    <flux:badge variant="pill" size="sm" >username<flux:badge.close /></flux:badge>
+                </div>
+
+                <flux:input size="sm" type="search" placeholder="{{ __('Search') }}" />
             </div>
 
-            <flux:input size="sm" type="search" placeholder="{{ __('Search') }}" />
-        </div>
+            <div class="overflow-y-auto">
+                @for ($index = 0; $index < 20; $index++)
+                    <flux:profile :name="'username'" :chevron="false" class="w-full" />
+                @endfor
 
-        <div class="overflow-y-auto">
-            @for ($index = 0; $index < 20; $index++)
-                <flux:profile :name="'username'" :chevron="false" class="w-full" />
-            @endfor
-
-            <flux:button variant="subtle" size="sm" class="w-full mt-2">{{ __('Load more') }}</flux:button>
+                <flux:button variant="subtle" size="sm" class="w-full mt-2">{{ __('Load more') }}</flux:button>
+            </div>
         </div>
     </flux:modal>
 
