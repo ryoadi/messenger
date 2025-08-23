f3f68f72f916555155008569407d1f67edaaca49
han <han@voxteneo.com>
2025-08-23 14:46:12 +0700

    refactor test structure

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,9 +11,9 @@
 |
 */
 
-pest()->extend(Tests\TestCase::class)
-    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature', 'Unit');
+pest()->extend(Tests\TestCase::class);
+pest()->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->in('Feature', 'Model');
 
 /*
 |--------------------------------------------------------------------------
